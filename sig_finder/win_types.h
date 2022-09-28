2fcfb541ee9856cf651739d3a493bbc367ced184
hasherezade <hasherezade@gmail.com>
2022-09-28 04:48:48 +0200

    [REFACT] In win_types: updated types definitions

--- sig_finder/win_types.h
+++ sig_finder/win_types.h
@@ -9,14 +9,14 @@
 #endif
 
 #ifdef _MSC_VER
-    typedef unsigned __int8 uint8_t;
-    typedef unsigned __int16 uint16_t;
-    typedef unsigned __int32 uint32_t;
-    typedef unsigned __int64 uint64_t;
-    typedef __int8 int8_t;
-    typedef __int16 int16_t;
-    typedef __int32 int32_t;
-    typedef __int64 int64_t;
+    typedef signed char        int8_t;
+    typedef short              int16_t;
+    typedef int                int32_t;
+    typedef long long          int64_t;
+    typedef unsigned char      uint8_t;
+    typedef unsigned short     uint16_t;
+    typedef unsigned int       uint32_t;
+    typedef unsigned long long uint64_t;
 #else
 # include <inttypes.h>
 #endif
