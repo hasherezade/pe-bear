73b3b257f3355dc95244a51758c49c1d3e1d6280
hasherezade <hasherezade@gmail.com>
2023-02-23 10:30:08 -0800

    [NOBIN] In macos_wrap: allow to supply the path to macdeployqt

--- macos_wrap.sh
+++ macos_wrap.sh
@@ -1,16 +1,20 @@
 #!/bin/bash
 
-#locate macdeployqt:
-MPATH=`whereis macdeployqt`
-echo $MPATH
+MCDEPLOY=$1
+echo $MCDEPLOY #the given path to macdeploy
+if [[ -z $MCDEPLOY ]]; then
+	#locate macdeployqt:
+	MPATH=`whereis macdeployqt`
+	echo $MPATH
 
-read P1 P2 <<<$(IFS=":"; echo $MPATH)
+	read P1 MCDEPLOY <<<$(IFS=":"; echo $MPATH)
 
-if [[ -z $P2 ]]; then
-	echo "macdeployqt not found"
-	exit -1
+	if [[ -z $MCDEPLOY ]]; then
+		echo "macdeployqt not found"
+		exit -1
+	fi	
 fi
-echo $P2
+
 APP_PATH="./build_qt5/pe-bear/"
 # clean the previous build
 rm -rf $APP_PATH/PE-bear.app
@@ -22,7 +26,7 @@ rm -rf $APP_PATH/PE-bear.app
 strip $APP_PATH/PE-bear.app/Contents/MacOS/PE-bear
 
 #wrap by macdeployqt:
-$P2 $APP_PATH/PE-bear.app
+$MCDEPLOY $APP_PATH/PE-bear.app
 
 # if wrapping succeeded, zip and store:
 if [[ $? == 0 ]]; then
@@ -34,4 +38,3 @@ if [[ $? == 0 ]]; then
 	echo "Wrapped: "$ZIP_OUT
 	cd $CURR_DIR
 fi
-
