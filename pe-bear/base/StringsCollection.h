d12e7da07566850c0f7ad7876124f8a0a67e9519
hasherezade <hasherezade@gmail.com>
2023-12-22 04:34:06 +0100

    [FEATURE] Extended class StringsCollection

--- pe-bear/base/StringsCollection.h
+++ pe-bear/base/StringsCollection.h
@@ -10,14 +10,38 @@ class StringsCollection : public QObject
 public:
 	StringsCollection() {}
 
+	bool insert(offset_t offset, const QString &str, bool isWide)
+	{
+		QMutexLocker lock(&myMutex);
+		stringsMap.insert(offset, str);
+		if (isWide) {
+			wideStrings.insert(offset);
+		}
+	}
+
+	void fill(StringsCollection &collection)
+	{
+		QMutexLocker lock1(&myMutex);
+		QMutexLocker lock2(&collection.myMutex);
+		_clear();
+		for (auto itr = collection.stringsMap.begin(); itr != collection.stringsMap.end(); ++itr) {
+			stringsMap.insert( itr.key(), itr.value() );
+			offset_t offset = itr.key();
+			if (collection._isWide(offset)) {
+				wideStrings.insert(offset);
+			}
+		}
+	}
+
 	bool fillStrings(QMap<offset_t, QString> *mapToFill)
 	{
 		if (!mapToFill) return false;
 		
 		QMutexLocker lock(&myMutex);
-		stringsMap.clear();
+		_clear();
 		for (auto itr = mapToFill->begin(); itr != mapToFill->end(); ++itr) {
-			stringsMap.insert( itr.key(), itr.value() );
+			offset_t offset = itr.key();
+			stringsMap.insert(offset, itr.value());
 		}
 		//qDebug() << "Extracted map: " << stringsMap.size();
 		return true;
@@ -31,17 +55,47 @@ public:
 		return itr.value();
 	}
 
-	QList<offset_t> getOffsets() const
+	QList<offset_t> getOffsets()
 	{
+		QMutexLocker lock(&myMutex);
 		return stringsMap.keys();
 	}
 	
-	size_t size() const
+	bool isWide(offset_t offset)
 	{
+		QMutexLocker lock(&myMutex);
+		return _isWide(offset);
+	}
+
+	size_t size()
+	{
+		QMutexLocker lock(&myMutex);
 		return stringsMap.size();
+	}
+
+	void clear()
+	{
+		QMutexLocker lock(&myMutex);
+		_clear();
 	}
 	
 protected:
+	void _clear()
+	{
+		stringsMap.clear();
+		wideStrings.clear();
+	}
+
+	bool _isWide(offset_t offset)
+	{
+		if (wideStrings.find(offset) != wideStrings.end()) {
+			return true;
+		}
+		return false;
+	}
+
 	QMap<offset_t, QString> stringsMap;
+	QSet<offset_t> wideStrings;
+
 	QMutex myMutex;
 };
