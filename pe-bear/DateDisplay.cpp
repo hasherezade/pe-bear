f4ad8775f34ec46ecd972a739efb56d61ed63991
hasherezade <hasherezade@gmail.com>
2024-01-18 13:27:35 +0100

    [BUGFIX] Do not convert the date to string if it is 0 or (-1)

--- pe-bear/DateDisplay.cpp
+++ pe-bear/DateDisplay.cpp
@@ -4,6 +4,9 @@
 
 QString getDateString(const quint64 timestamp)
 {
+	if (timestamp == 0 || timestamp == (-1)) {
+		return "";
+	}
 	const time_t rawtime = (const time_t)timestamp;
 	QString format = "dddd, dd.MM.yyyy hh:mm:ss";
 	QDateTime date1(QDateTime(QDateTime::fromTime_t(rawtime)));
